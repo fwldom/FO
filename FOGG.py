2228bd5cf9f1d03e7b23b0e64935a0a0b766e589
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-10 22:17:17 +0330

    F

--- FOGG.py
+++ FOGG.py
@@ -3,7 +3,7 @@ num = int(turtle.textinput(title="Enter Number Forward", prompt=("Enter Forward 
 numr1 = int(turtle.textinput(title="Enter Number Range", prompt=("Enter Number Range Main !")))
 numr2 = int(turtle.textinput(title="Enter Number Range", prompt=("Enter Number on Range Main !")))
 num2 = int(turtle.textinput(title="Enter Number left", prompt=("Enter left Range !")))
-num3 = int(turtle.textinput(title="Enter Number left", prompt=("Enter left End Range  !  ")))
+num3 = int(turtle.textinput(title="Enter Number Right", prompt=("Enter left End Range  !  ")))
 width = int(turtle.textinput(title="Enter Number width", prompt=("Enter width !  ")))
 laki = turtle.Turtle()
 laki.shape('turtle')
