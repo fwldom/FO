320ea9b740cef124f32b0d1dde204c00c2f8e129
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-15 19:35:00 +0330

    F

--- Setup.py
+++ Setup.py
@@ -1,7 +1,17 @@
 from os import *
-system("mkdir C:\\Apps\\fwldom-os")
-system("mkdir C:\\Apps\\fwldom-os\\Tools")
-
-cwd = getcwd()
-system(f"xcopy {cwd}\\Data\\*.* C:\\Apps\\fwldom-os")
-system("python \"C:\\Apps\\fwldom-os\\Fwldom os.py\"")
+from time import *
+IsFile = path.isfile("\"C:\\Apps\\fwldom-os\\Fwldom os.py\"")
+#IsFile = "True"
+if IsFile == True:
+    print(IsFile)
+   # sleep(5)
+    chdir("C:/Apps/fwldom-os/")
+    system("python \"Fwldom os.py\"")
+if IsFile == False:
+    print(IsFile)
+    sleep(5)
+    system("mkdir C:\\Apps\\fwldom-os")
+    system("mkdir C:\\Apps\\fwldom-os\\Tools")
+    cwd = getcwd()
+    system(f"xcopy {cwd}\\Data\\*.* C:\\Apps\\fwldom-os")
+    system("python \"C:\\Apps\\fwldom-os\\Fwldom os.py\"")
